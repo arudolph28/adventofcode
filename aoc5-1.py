911d48b0c6007b6c4fe1618f8ac80bb550ca64bf
arudolph28 <150281807+arudolph28@users.noreply.github.com>
2025-12-05 18:25:35 -0800

    Add files via upload

---
+++ aoc5-1.py
@@ -0,0 +1,20 @@
+dates = '''3-5
+10-14
+16-20
+12-18'''
+
+ids = '''1
+5
+8
+11
+17
+32'''
+
+ids = ids.split('\n')
+dates = dates.split('\n')
+for i in range(len(dates)):
+    dates[i] = dates[i].split('-')
+
+
+
+
